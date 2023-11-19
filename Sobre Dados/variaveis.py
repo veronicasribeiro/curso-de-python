24c3eb4908d6ac5f2e2dac48c4bf97b7e240d586
Dell <veronicaribeiroofficial@gmail.com>
2023-11-19 19:27:23 -0200

    Variáveis em Python

---
+++ Sobre Dados/variaveis.py
@@ -0,0 +1,30 @@
+velocidade_internet = 10
+print(velocidade_internet)
+# Quais os tipos de dado que podemos armazenar no computador? Tipos Primitivos
+# Números -> velocidade_internet = 10
+# Valores Boleanos -> esta_aberta = True  (True or  False)
+# Strings -> slogan = 'Feito é melhor que perfeito!' (aspas duplas também funcionam)
+
+# ampac
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Saber qual o TIPO d variável
+print(type(a))
+print(type(velocidade_internet))
+
+# DESAFIO 🏆
+# Copie e cole as seguintes linhas de código para seu editor de codigo e descubra qual
+# é o tipo de cada um desses dados
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = "Bom dia!"
+variavel_4 = 15
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
